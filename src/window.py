997823523f2a3a1249987bc4598254eb93ab0d49
fenroe <scrambles9494@gmail.com>
2024-09-28 21:46:35 +0300

    Feat: wrote redraw, wait_for_close and close methods of window class, expanded window constructor. fixed general errors. updated main.py

--- src/window.py
+++ src/window.py
@@ -2,10 +2,21 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.root_widget = Tk()
-        self.root_widget.title = "Maze solver"
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.window_is_running = False
+        self.__root_widget = Tk()
+        self.__root_widget.title("Maze Solver")
+        self.__canvas = Canvas(self.__root_widget, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__window_is_running = False
+        self.__root_widget.protocol("WM_DELETE_WINDOW", self.close)
+    
+    def redraw(self):
+        self.__root_widget.update_idletasks()
+        self.__root_widget.update()
+    
+    def wait_for_close(self):
+        self.__window_is_running = True
+        while self.__window_is_running:
+            self.redraw()
+    
+    def close(self):
+        self.__window_is_running = False
