204b6c1bd4b823eee3977c0f1fc8cc0554fdcc54
fenroe <scrambles9494@gmail.com>
2024-09-28 22:08:10 +0300

    Feat: wrote line class, added draw_line method for window class, updated main file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 from window import Window
+from point import Point
+from line import Line
 
 def main():
     print("Hi banana!")
     win = Window(800, 600)
+    point_1 = Point(69, 23)
+    point_2 = Point(100, 50)
+    line = Line(point_1, point_2)
+    win.draw_line(line, "pink")
     win.wait_for_close()
 
 if __name__ == "__main__":
